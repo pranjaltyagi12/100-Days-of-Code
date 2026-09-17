5ec1e84d23ce990ec876ae5c91bf5b2bfd2b0894
Pranjal Tyagi <Pranjal.38364@stu.upes.ac.in>
2026-09-17 23:15:01 +0530

    Add files via upload

---
+++ D10_Q2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter days : ");
+    scanf("%d",&a);
+
+    switch(a)
+   { case 1 : 
+    printf("Monday");
+    break;
+
+    case 2 :
+    printf("Tuesday");
+    break;
+
+    case 3 :
+    printf("Wednesday");
+    break;
+
+    case 4 :
+    printf("Thrusday");
+    break;
+
+    case 5 :
+    printf("Friday");
+    break;
+
+    case 6 :
+    printf("Saturday");
+    break;
+
+    case 7 :
+    printf("Sunday");
+    break;
+
+    default :
+    printf("Invalid day");
+   }
+    return 0;
+
+}
